0053f91e93dd77b33b99f2b975bd714b4b439c76
Asad Ismail <asadismaeel@gmail.com>
2021-03-04 15:34:24 +0100

    added cpp inference for semantic segmentation

--- cpp-inference/inference.sh
+++ cpp-inference/inference.sh
@@ -1,3 +1,3 @@
 build/segmentation /home/asad/projs/lane_detection/cluster_quantized_weights.tflite \
 /home/asad/projs/lane_detection/cpp-inference/labels.txt \
-/home/asad/mai_99.png
+/home/asad/73.png
